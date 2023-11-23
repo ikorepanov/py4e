ddd7690ddeb446f44fd83f40d964f37db266dd5b
Ilia Korepanov <ikorepanov@gmail.com>
2023-11-23 11:17:58 +0700

    Add quit() variants

--- 03_conditional/ex_3.2.py
+++ 03_conditional/ex_3.2.py
@@ -7,3 +7,17 @@ try:
     print(f'Pay: {payment}')
 except ValueError:
     print('Error, please enter numeric input')
+
+
+# Second variant with 'quit()'
+try:
+    h = float(input('Enter Hours: '))
+    r = float(input('Enter Rate: '))
+except ValueError:
+    print('Error, please enter numeric input')
+    quit()
+
+if h > 40:
+    h = 40 + (h - 40) * 1.5
+payment = round(h * r, 2)
+print(f'Pay: {payment}')
--- 03_conditional/ex_3.3.py
+++ 03_conditional/ex_3.3.py
@@ -1,17 +1,39 @@
+# score = input('Enter score: ')
+# try:
+#     score = float(score)
+#     if 0.0 <= score < 0.6:
+#         print('F')
+#     elif score < 0.7:
+#         print('D')
+#     elif score < 0.8:
+#         print('C')
+#     elif score < 0.9:
+#         print('B')
+#     elif score <= 1:
+#         print('A')
+#     else:
+#         print('Bad score')
+# except ValueError:
+#     print('Bad score')
+
+# Second variant with 'quit()'
 score = input('Enter score: ')
+
 try:
-    score = float(score)
-    if 0.0 <= score < 0.6:
-        print('F')
-    elif score < 0.7:
-        print('D')
-    elif score < 0.8:
-        print('C')
-    elif score < 0.9:
-        print('B')
-    elif score <= 1:
-        print('A')
-    else:
-        print('Bad score')
+    s = float(score)
 except ValueError:
+    print('Bad score')
+    quit()
+
+if 0.0 <= s < 0.6:
+    print('F')
+elif s < 0.7:
+    print('D')
+elif s < 0.8:
+    print('C')
+elif s < 0.9:
+    print('B')
+elif s <= 1:
+    print('A')
+else:
     print('Bad score')
