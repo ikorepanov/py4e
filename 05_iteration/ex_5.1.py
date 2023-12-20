2605cf12a8da4b1ae9c61203c643fe34d0e14252
Ilia Korepanov <ikorepanov@gmail.com>
2023-12-20 11:28:20 +0700

    Add 5.1 final solution

--- 05_iteration/ex_5.1.py
+++ 05_iteration/ex_5.1.py
@@ -1,8 +1,13 @@
 def some():
     """
-    Enter docstring here(!).
+    Repeatedly read numbers until the user enters "done".
+    Once "done" is entered, print out the total, count, and average of the
+    numbers. If the user enters anything other than a number, detect their
+    mistake using try and except and print an error message and skip to the
+    next number.
     """
-    # number = None  # Первый вариант. Работает!
+# Первый вариант
+    # number = None
     # total = 0
     # count = 0
     # average = 0
@@ -19,21 +24,40 @@ def some():
     #         continue
     # print(int(total), count, average)
 
-    number = None  # Второй вариант. Работает!
-    total = 0
+# Второй вариант
+    # number = None
+    # total = 0
+    # count = 0
+    # average = 0
+    # while number != 'done':
+    #     number = input('Enter a number: ')
+    #     if number != 'done':
+    #         try:
+    #             n = float(number)
+    #         except ValueError:
+    #             print('Invalid input')
+    #             continue
+    #         total = total + n
+    #         count = count + 1
+    #         average = total / count
+    # print(int(total), count, average)
+
+# Третий вариант (на основе Dr. Chuck's solution)
     count = 0
-    average = 0
-    while number != 'done':
+    total = 0
+    average = 0  # Важно ввести эту переменную, т.к. тогда не будет ошибки ZeroDivisionError, если сразу ввести 'done'.
+    while True:
         number = input('Enter a number: ')
-        if number != 'done':
-            try:
-                n = float(number)
-            except ValueError:
-                print('Invalid input')
-                continue
-            total = total + n
-            count = count + 1
-            average = total / count
+        if number == 'done':
+            break
+        try:
+            n = float(number)
+        except ValueError:
+            print('Invalid input')
+            continue
+        total = total + n
+        count = count + 1
+        average = total / count
     print(int(total), count, average)
 
 
